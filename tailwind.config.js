5789f18c836b41d9486b839c7218596b8f106aa7
bikash sarker <bikash.webdeveloper5@gmail.com>
2024-09-23 11:24:55 +0600

    the header part desgin is finished

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      primary: "#B4F461",
+      "header-bg": "#FEF0D5",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
